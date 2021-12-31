b1c009173069866905dc22bf6e43c71c5e9a3aec
Amirkhodadadi <amirkhodadadi0084@gmail.com>
2021-12-31 10:58:51 +0330

    increase pixel num and added new colors

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ let btn = document.querySelector(".change");
 let btnadd = document.querySelector(".add");
 let i = -1;
 let ii = 0;
-let haveColor = ['red' , 'blue' , 'green'];
+let haveColor = ['red' , 'blue' , 'green' , 'yellow' , 'purple' , 'pink' , 'white' , 'black'];
+function check() {
+    if (i == 99) { cube = 0; };
+};
 function Add() {
     cube.innerHTML+=`<div class='parts' style='background-color:${haveColor[ii]};'>
     </div>`;
     i++;
     check();
 };
-btnadd.addEventListener('click' , Add);
-function check() {
-    if (i == 24) { btnadd.removeEventListener('click' , Add) };
-};
-btn.addEventListener('click' , function(){
+function moz() { 
     let parts = document.querySelectorAll(".parts");
     ii++
-    if (ii == 3) { ii= 0; };
+    if (ii == 8) { ii= 0; };
     parts[i].style.backgroundColor = haveColor[ii];
-    
-})
+}
+window.addEventListener('keydown' , (event) => {
+    if (event.keyCode == 65) {
+        Add();
+    }
+    else if (event.keyCode == 67) { moz() }
+});
+btnadd.addEventListener('click' , Add);
+btn.addEventListener('click' , moz);
