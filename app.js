ddd5029e0a7cab17a9fca523fdc6a7fc986fd29d
Amirkhodadadi <amirkhodadadi0084@gmail.com>
2021-12-31 16:33:16 +0330

    added remove and reset button

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 let cube = document.querySelector(".cube");
 let btn = document.querySelector(".change");
 let btnadd = document.querySelector(".add");
+let btnrem = document.querySelector(".remove");
+let btnres = document.querySelector(".reset");
 let i = -1;
 let ii = 0;
 let haveColor = ['red' , 'blue' , 'green' , 'yellow' , 'purple' , 'pink' , 'white' , 'black'];
 function check() {
-    if (i == 99) { cube = 0; };
+    if (i > 99) { alert("Done!") };
 };
 function Add() {
     cube.innerHTML+=`<div class='parts' style='background-color:${haveColor[ii]};'>
     </div>`;
     i++;
     check();
+    console.log(i);
 };
 function moz() { 
     let parts = document.querySelectorAll(".parts");
@@ -27,3 +30,10 @@ window.addEventListener('keydown' , (event) => {
 });
 btnadd.addEventListener('click' , Add);
 btn.addEventListener('click' , moz);
+function remover() {
+    cube.removeChild(cube.lastChild);
+    i--;
+    console.log(i);
+}
+btnrem.addEventListener('click' , remover)
+btnres.addEventListener('click' , function(){ cube.innerHTML = ``; i = -1; })
